f6a53c36486e4287c7ad2c0e13cdae5813200e19
Denis <fpv.airplane.barnaul@gmail.com>
2020-08-20 12:11:02 +0300

    set resolution and framerate parameters

--- main.py
+++ main.py
@@ -2,14 +2,20 @@
 import cv2
 
 cap = cv2.VideoCapture(0)
+cap.set(3,1280)
+cap.set(4,720)
+cap.set(5,60)
 width  = int(cap.get(3))
 height = int(cap.get(4))
+print("width=",width)
 
 
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
-
+    #print(ret)
+    #print(type(ret))
+    #print(ret)
     # Our operations on the frame come here
     #col = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
 
